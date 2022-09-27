054fd1c9153b671032a2c44451554793a695a472
JereLew <jeremiahhclew@gmail.com>
2022-09-27 11:59:23 -0400

    This is my commit for assignment #1.

--- lsplewis/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ lsplewis/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,5 +1,12 @@
 package org.howard.edu.lsp.assignment1;
 
+/**
+ * Jeremiah Lewis
+ * 
+ * @author jeremiahlewis
+ *
+ *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
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
