56e71e6f29de5b6d5e5f2f2a45366103915b08f3
JereLew <jeremiahhclew@gmail.com>
2022-10-28 00:10:03 -0400

    Commit for Midterm

--- lsplewis/src/org/howard/edu/lsp/midterm/problem1/VotingMachine.java
+++ lsplewis/src/org/howard/edu/lsp/midterm/problem1/VotingMachine.java
@@ -1,10 +1,74 @@
 package org.howard.edu.lsp.midterm.problem1;
 
+import java.util.Scanner;
+
 public class VotingMachine {
+	static Scanner sc = new Scanner(System.in);
+	public void main(String[] args) {
+		int choice = 0;
+		HashMap<String, Integer> votes = new LinkedHashMap<> ();
+		do {
+			System.out.println("1. Add Candidate");
+			System.out.println("2. Cast Vote");
+			System.out.println("3. View results");
+			System.out.println("4. Exit");
+			System.out.println("Enter your choice");
+			choice = sc.nextInt();
+			switch(choice)
+			{
+			case 1: sc.nextLine();
+					addCandidate(votes);
+				break;
+			case 2: castVote(votes);
+				break;
+			case 3: printResults(votes);
+				break;
+			case 4: break;
+			default : System.out.println("Invalid choice");
+			break;
+			}
+		}while(choice != 4);
+	}
+	public static void addCandidate(HashMap<String, Integer> votes)
+	{
+		System.out.println("Enter the name of the candidate");
+		String name = sc.nextLine();
+		votes.put(name, 0);
+	}
+	public static void castVote(HashMap<String, Integer> votes)
+	{
+		System.out.println("Below is the list of candidates");
+		for(String n : votes.keySet())
+		{
+			System.out.println(n);
+		}
+		System.out.println("Enter candidate name to cast vote:");
+		sc.nextLine();
+		String candname = sc.nextLine();
+		if(votes.containsKey(candname))
+		{
+			int vote = votes.get(candname);
+			vote++;
+			votes.put(candname, vote);
+		}
+		else
+		{
+			System.out.println("Enter valid candidate name.");
+		}
+	}
+	public static void printResults(HashMap<String, Integer> votes)
+	{
+		System.out.println("Below are the votes each candidates got: ");
+		if(votes.size() == 0)
+		{
+			System.out.println("Voter list is empty.");
+		}
+		for (Map.Entry<String,Integer> entry : votes.entrySet()) {
+			String key = entry.getKey();
+			int value = entry.getValue();
+			System.out.println(key + " got "+ value + " votes.");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		}
 	}
 
 }
--- lsplewis/src/org/howard/edu/lsp/midterm/problem2/Person.java
+++ lsplewis/src/org/howard/edu/lsp/midterm/problem2/Person.java
@@ -1,10 +1,29 @@
 package org.howard.edu.lsp.midterm.problem2;
 
 public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name;
+	private int age;
+	private String socialSecurityNumber; 
+	
+	public Person (String name, String socialSecurityNumber, int age) {
+		this.name = name;
+		this.socialSecurityNumber = socialSecurityNumber;
+		this.age = age;
 	}
-
+	
+	@Overriide
+	public String toString() {
+		return "name: " + name + ", socialSecurityNumber: " + socialSecurityNumber + ",Age: " + age; 
+	}
+	
+	public class TestPerson {
+		public static void main(String[] args) {
+			Person firstp = new Person("name1", "111-11-1111", 32);
+			Person secondp = new Person("name2", "111-11-1111", 24);
+			System.out.println(firstp);
+			System.out.println(secondp);
+			System.out.println(firstp.equals(secondp));
+		}
+	}
 }
+
